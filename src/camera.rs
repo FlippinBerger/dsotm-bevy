ed1f58fca9117e98907504dbea3bd7ed8e59ca6f
Chris Berger <flippinberger@gmail.com>
2024-03-22 15:51:27 -0600

    Implement simple GodMode camera to look around the scene during development

--- src/camera.rs
+++ src/camera.rs
@@ -3,19 +3,21 @@ use bevy::prelude::*;
 use crate::player::Player;
 use crate::state::GameState;
 
-const CAMERA_DISTANCE: f32 = 24.0;
+const CAMERA_DISTANCE: f32 = 24.;
+const GOD_MODE_CAMERA_SPEED: f32 = 30.;
 
 #[derive(Component)]
 struct MainCamera;
 
 #[derive(Component)]
-struct DevCamera;
+struct GodCamera;
 
 pub struct CameraPlugin;
 
 impl Plugin for CameraPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_cameras);
+        app.add_systems(Startup, spawn_cameras)
+            .add_systems(Update, (god_mode_toggle, god_cam_move, main_cam_move));
     }
 }
 
@@ -35,7 +37,7 @@ fn spawn_cameras(mut commands: Commands) {
                 .looking_at(Vec3::ZERO, Vec3::Y),
             ..Default::default()
         },
-        DevCamera,
+        GodCamera,
     ));
 }
 
@@ -43,7 +45,7 @@ fn spawn_cameras(mut commands: Commands) {
 // press spacebar to get back to main camera
 fn god_mode_toggle(
     mut main_cam_q: Query<(&mut Camera, &Transform), With<MainCamera>>,
-    mut dev_cam_q: Query<(&mut Camera, &mut Transform), With<DevCamera>>,
+    mut god_cam_q: Query<(&mut Camera, &mut Transform), (With<GodCamera>, Without<MainCamera>)>,
     keys: Res<ButtonInput<KeyCode>>,
     state: Res<State<GameState>>,
     mut next_state: ResMut<NextState<GameState>>,
@@ -51,25 +53,30 @@ fn god_mode_toggle(
     // Run all of this off pressing the key code.
     if keys.just_pressed(KeyCode::KeyP) {
         // get the 2 cameras and their transforms
-        let (mut main, main_trans) = main_cam_q.single_mut();
-        let (mut dev, mut dev_trans) = dev_cam_q.single_mut();
+        let (mut main, main_t) = main_cam_q.single_mut();
+        let (mut god, mut god_t) = god_cam_q.single_mut();
 
         match state.get() {
             GameState::GodMode => {
                 // set cameras back together and go back to main camera in Level Mode
                 next_state.set(GameState::Level);
-                *dev_trans = *main_trans;
+                main.is_active = true;
+                god.is_active = false;
             }
             _ => {
                 next_state.set(GameState::GodMode);
+                main.is_active = false;
+                god.is_active = true;
+                *god_t = *main_t;
             }
         }
     }
 }
 
-fn dev_cam_move(
+fn god_cam_move(
     state: Res<State<GameState>>,
-    mut cam: Query<&mut Transform, With<DevCamera>>,
+    time: Res<Time>,
+    mut cam: Query<&mut Transform, With<GodCamera>>,
     keys: Res<ButtonInput<KeyCode>>,
 ) {
     let curr_state = state.get();
@@ -77,13 +84,28 @@ fn dev_cam_move(
         return;
     }
 
+    let mut cam_t = cam.single_mut();
+
     // impl movement
+    if keys.pressed(KeyCode::ArrowLeft) {
+        cam_t.translation.x -= GOD_MODE_CAMERA_SPEED * time.delta_seconds();
+    }
+
+    if keys.pressed(KeyCode::ArrowRight) {
+        cam_t.translation.x += GOD_MODE_CAMERA_SPEED * time.delta_seconds();
+    }
+    if keys.pressed(KeyCode::ArrowDown) {
+        cam_t.translation.y -= GOD_MODE_CAMERA_SPEED * time.delta_seconds();
+    }
+    if keys.pressed(KeyCode::ArrowUp) {
+        cam_t.translation.y += GOD_MODE_CAMERA_SPEED * time.delta_seconds();
+    }
 }
 
 fn main_cam_move(
     state: Res<State<GameState>>,
     mut cam: Query<&mut Transform, With<MainCamera>>,
-    player_trans: Query<&Transform, With<Player>>,
+    player_trans: Query<&Transform, (With<Player>, Without<MainCamera>)>,
 ) {
     let curr_state = state.get();
     if *curr_state != GameState::Level {
